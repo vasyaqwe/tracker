7a1628406b34d7185ed333c396dac43cce2c5e03
vasyaqwe <elekton.fafik@gmail.com>
2024-11-11 18:00:05 +0200

    add og image

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -50,7 +50,7 @@ export const Route = createRootRouteWithContext<{
          { name: "og:type", content: "website" },
          { name: "og:title", content: title },
          { name: "og:description", content: description },
-         // { name: "og:image", content: "https://trackerhq.vercel.app/og.png" },
+         { name: "og:image", content: "https://trackerhq.vercel.app/og.png" },
          { name: "twitter:card", content: "summary_large_image" },
          { name: "twitter:creator", content: "@vasyaqwee" },
          { name: "twitter:site", content: "@vasyaqwee" },
