1722a81ad1cd41c21c1b55d15f2314c3c4232c96
vasyaqwe <elekton.fafik@gmail.com>
2025-03-31 23:13:45 +0300

    upd api url

--- app/invoice/functions.ts
+++ app/invoice/functions.ts
@@ -21,27 +21,24 @@ export const generate = createServerFn({ method: "GET" })
       ),
    )
    .handler(async ({ context, data }) => {
-      const res = await fetch(
-         "https://vasyaqwe-production.up.railway.app/invoice/generate",
-         {
-            method: "POST",
-            headers: {
-               "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-               user: {
-                  name: context.user.name,
-                  email: context.user.email,
-               },
-               customer: {
-                  name: data.customer.name,
-                  email: data.customer.email,
-               },
-               amount: data.amount,
-               selectedItems: data.selectedItems,
-            }),
+      const res = await fetch("https://api.vasyaqwe.com/invoice/generate", {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json",
          },
-      )
+         body: JSON.stringify({
+            user: {
+               name: context.user.name,
+               email: context.user.email,
+            },
+            customer: {
+               name: data.customer.name,
+               email: data.customer.email,
+            },
+            amount: data.amount,
+            selectedItems: data.selectedItems,
+         }),
+      })
       const result = (await res.json()) as {
          base64Pdf: string
          fileName: string
